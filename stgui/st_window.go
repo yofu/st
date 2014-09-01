588d9ff3d5721496c24801d80f2ec46f57f79436
Yoshihiro Fukushima <y.of.u.142857@gmail.com>
2014-09-01 10:06:31 +0900

    fix ReadPgp: close file

--- stgui/st_window.go
+++ stgui/st_window.go
@@ -4982,6 +4982,7 @@ func ReadPgp(filename string, aliases map[string]*Command) error {
     if err != nil {
         return err
     }
+    defer f.Close()
     s := bufio.NewScanner(f)
     for s.Scan() {
         txt := s.Text()
