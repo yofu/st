0320c09acc133fab1100198d6d7abc2a6024e431
Yoshihiro Fukushima <y.of.u.142857@gmail.com>
2015-01-13 17:36:53 +0900

    add exmode:save fn mkdir

--- stgui/st_window.go
+++ stgui/st_window.go
@@ -3441,6 +3441,9 @@ func (stw *Window) exmode(command string) error {
 				return st.NotEnoughArgs(":save")
 			} else {
 				if bang || (!st.FileExists(fn) || stw.Yn("Save", "上書きしますか")) {
+					if narg > 2 && strings.EqualFold(args[2], "mkdir") {
+						os.MkdirAll(filepath.Dir(fn), 0644)
+					}
 					var err error
 					if stw.SelectElem != nil && len(stw.SelectElem) > 0 {
 						err = stw.SaveFileSelected(fn, stw.SelectElem)
