f8b57ff878a1361ae1c623b908fe5f8de5418feb
yofu <y.of.u.142857@gmail.com>
2016-02-24 22:49:55 +0900

    fix Typewrite: set fill size according to fontsize&coord

--- driver/shiny/st_window.go
+++ driver/shiny/st_window.go
@@ -380,7 +380,7 @@ func (stw *Window) Typewrite(x, y float64, str string) {
 	}
 	commandtexture = t
 	t.Upload(image.Point{}, commandbuffer, commandbuffer.Bounds())
-	stw.window.Fill(image.Rect(20, 690, 500, 705), color.RGBA{0x33, 0x33, 0x33, 0xff}, screen.Over)
+	stw.window.Fill(image.Rect(int(x - 5), int(y - float64(stw.fontHeight) - 5), 500, int(y + 5)), color.RGBA{0x33, 0x33, 0x33, 0xff}, screen.Over)
 	stw.window.Copy(image.Point{0, 0}, commandtexture, commandtexture.Bounds(), screen.Over, nil)
 	stw.window.Publish()
 }
