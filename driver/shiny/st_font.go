bfcfc0216cf443def12a3ab9b8d013db24f71474
yofu <y.of.u.142857@gmail.com>
2016-03-27 21:54:08 +0900

    fix driver/shiny for 6b51665

--- driver/shiny/st_font.go
+++ driver/shiny/st_font.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 
 	"github.com/golang/freetype/truetype"
+	"github.com/yofu/st/stlib"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/basicfont"
 	"golang.org/x/image/math/fixed"
@@ -12,6 +13,7 @@ import (
 
 type Font struct {
 	face   font.Face
+	name   string
 	height fixed.Int26_6
 	color  color.RGBA
 }
@@ -36,11 +38,47 @@ func LoadFontFace(path string, point float64) (*Font, error) {
 			Size:    point,
 			Hinting: font.HintingFull,
 		}),
+		name: path,
 		height: fixed.Int26_6(int(point*3) >> 2), // * 72/96
 		color: color.RGBA{0xff, 0xff, 0xff, 0xff},
 	}, nil
 }
 
+func (f *Font) Face() string {
+	return f.name
+}
+
+func (f *Font) SetFace(face string) {
+	f2, err := LoadFontFace(face, float64(f.height)*4.0/3.0)
+	if err != nil {
+		return
+	}
+	f.face = f2.face
+	f.name = f2.name
+	f.height = f2.height
+	f.color = f2.color
+}
+
 func (f *Font) Size() int {
 	return int(f.height)
+}
+
+func (f *Font) SetSize(s int) {
+	f2, err := LoadFontFace(f.name, float64(s))
+	if err != nil {
+		return
+	}
+	f.face = f2.face
+	f.name = f2.name
+	f.height = f2.height
+	f.color = f2.color
+}
+
+func (f *Font) Color() int {
+	return int(f.color.R) * 65536 + int(f.color.G) * 256 + int(f.color.B)
+}
+
+func (f *Font) SetColor(c int) {
+	col := st.IntColorList(c)
+	f.color = color.RGBA{uint8(col[0]), uint8(col[1]), uint8(col[2]), 0xff}
 }
